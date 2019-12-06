aae559059136b4b423a64e48cc6ee7311caf2896
Dorian <dorian.guilleminot@gmail.com>
2019-12-06 14:04:24 +0100

    1.0.3

--- eyeglass-exports.js
+++ eyeglass-exports.js
@@ -4,7 +4,7 @@ var path = require("path");
 
 module.exports = function(eyeglass, sass) {
   return {
-    sassDir: path.join(__dirname, "sass"),
+    sassDir: path.join(__dirname, 'build/index.css'),
     functions: {
       "hello($name: 'World')": function(name, done) {
         done(sass.types.String("Hello, " + name.getValue()));
